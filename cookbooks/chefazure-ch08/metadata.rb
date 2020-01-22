40da2d62c1006e578857cb3fa792cd77cf374a48
alim <alim@ubuntu14vm.chef.com>
2020-01-22 17:37:02 +0200

    first commit

--- cookbooks/chefazure-ch08/metadata.rb
+++ cookbooks/chefazure-ch08/metadata.rb
@@ -5,8 +5,6 @@ license 'all_rights'
 description 'Installs/Configures chefazure-ch08'
 long_description 'Installs/Configures chefazure-ch08'
 version '0.1.7'
-#version '0.1.2'
-
 depends 'jenkins'
 
 
